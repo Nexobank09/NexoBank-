8d41f5c3102a0465241317159a177323095af65f
Nexobank09 <nexobank09@gmail.com>
2026-03-23 10:35:43 +0100

    Create page.tsx

---
+++ src/app/src/app/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <html>
+      <head>
+        <title>NexoBank - Dashboard</title>
+      </head>
+      <body>
+        <h1>Bienvenue sur NexoBank</h1>
+        <p>Votre banque digitale internationale</p>
+      </body>
+    </html>
+  )
+}
